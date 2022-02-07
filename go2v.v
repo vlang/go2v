2758350a56c877bce629abdf3c507c2c31092a53
JalonSolov <JalonSolov@gmail.com>
2022-02-07 12:46:46 -0500

    feat: add test creation options

--- go2v.v
+++ go2v.v
@@ -4,9 +4,21 @@ import os
 import cli
 import transpiler
 
+const (
+	v_exe     = @VEXE
+	go2v_path = @VMODROOT
+)
+
 fn main() {
 	mut app := cli.Command{
 		name: 'go2v'
+		flags: [
+			cli.Flag{
+				flag: .bool
+				name: 'help'
+				abbrev: 'h'
+			},
+		]
 		description: 'Go2V is a CLI utility to transpile Go source code into V source code.'
 		execute: fn (cmd cli.Command) ? {
 			if cmd.args.len == 0 { println('At least one file or directory must be given, see "go2v help"') } else { transpiler.go_to_v(cmd.args[0], cmd.args[2] or {
@@ -26,18 +38,69 @@ fn main() {
 			},
 			cli.Command{
 				name: 'test'
+				flags: [
+					cli.Flag{
+						flag: .bool
+						name: 'compact'
+						description: 'do not output build stats'
+					},
+					cli.Flag{
+						flag: .string
+						name: 'create'
+						description: 'create new test'
+					},
+					cli.Flag{
+						flag: .string
+						name: 'out'
+						description: 'save output of test'
+					}
+				]
 				execute: fn (cmd cli.Command) ? {
-					mut stats := ''
-					if cmd.args.len == 0 {
-						stats = '-stats'
-					} else if cmd.args[0] == '-compact' {
-						stats = ''
-					} else {
-						println('At least one file or directory must be given, see "go2v help test"')
+					compact := cmd.flags.get_bool('compact') ?
+					create := cmd.flags.get_string('create') ?
+					out := cmd.flags.get_string('out') ?
+					if create != '' && out != '' {
+						println('Cannot use -create and -out at the same time')
 						return
 					}
 
-					os.execvp('${@VEXE}', [stats, 'test', os.resource_abs_path('.')]) ?
+					if create != '' {
+						if os.exists('$go2v_path/tests/$create') {
+							println('$go2v_path/tests/$create already exists - remove it if you want to create a new test with the same name')
+							return
+						}
+						os.mkdir('$go2v_path/tests/$create') ?
+						os.write_file('$go2v_path/tests/$create/${create}.go', 'package main\n') ?
+						println('$go2v_path/tests/$create/${create}.go created')
+					} else if out != '' {
+						if os.is_dir('$go2v_path/tests/$out/${out}.vv') {
+							println('$go2v_path/tests/$out/${out}.vv is a directory - remove it before trying to save output')
+							return
+						}
+						mut go2v_exe := ''
+						$if windows {
+							go2v_exe = '$go2v_path/go2v.exe'
+						} $else {
+							go2v_exe = '$go2v_path/go2v'
+						}
+						if os.exists(go2v_exe) {
+							os.execute('$go2v_exe $go2v_path/tests/$out/${out}.go -o $go2v_path/tests/$out/${out}.vv')
+						} else {
+							os.execute('${@VEXE} run $go2v_path $go2v_path/tests/$out/${out}.go -o $go2v_path/tests/$out/${out}.vv')
+						}
+						println('$go2v_path/tests/$out/${out}.vv saved')
+					} else if compact {
+						os.execvp('${@VEXE}', [
+							'test',
+							os.resource_abs_path(cmd.args[0]),
+						]) ?
+					} else {
+						os.execvp('${@VEXE}', [
+							'-stats',
+							'test',
+							os.resource_abs_path(cmd.args[0]),
+						]) ?
+					}
 					return
 				}
 			},
