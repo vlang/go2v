25671e5a3ce8c6e7279787ce6029f30feadfbf7b
Hunam <hunam@disroot.org>
2022-02-27 12:51:19 +0100

    get_ast.go cleanup
    Signed-off-by: Hunam <hunam@disroot.org>

--- transpiler/get_ast.go
+++ transpiler/get_ast.go
@@ -11,16 +11,11 @@ import (
 func main() {
 	// parsing flags with none defined just makes input parameters start @ 0
 	flag.Parse()
-	path := flag.Arg(0)
 
-	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
+	file_node, err := parser.ParseFile(token.NewFileSet(), flag.Arg(0), nil, parser.ParseComments)
 	if err != nil {
 		panic(err)
 	}
 
-	f, _ := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0600)
-	defer f.Close()
-
-	ast.Fprint(os.Stdout, fset, node, ast.NotNilFilter)
+	ast.Fprint(os.Stdout, nil, file_node, ast.NotNilFilter)
 }
