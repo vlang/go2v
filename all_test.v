c61f396aeded46c3b417e3f03e06bbe8cdf74b00
Delyan Angelov <delian66@gmail.com>
2022-07-18 21:57:23 +0300

    make test failures more actionable, also skip empty folders

--- all_test.v
+++ all_test.v
@@ -28,33 +28,43 @@ fn test_all() ? {
 
 	for tidx, dir in all_tests {
 		start := '${tidx + 1:3}/${all_tests.len:-2}'
-		go_source := os.quoted_path('$go2v_path/tests/$dir/${dir}.go') // `path/to/test_name.go`
-		produced_vv_source := '$go2v_path/tests/$dir/out.vv' // `path/to/out.vv`
-		expected_vv_source := '$go2v_path/tests/$dir/${dir}.vv' // `path/to/test_name.vv`
-		relative_expected_vv_source := 'tests/$dir/${dir}.vv' // `relative/path/to/test_name.vv`
-		res := os.execute('$go2v_exe $go_source -o ${os.quoted_path(produced_vv_source)}')
+
+		produced_vv_source := os.join_path(os.temp_dir(), 'out.vv')
+
+		common_relative_path := 'tests/$dir/$dir'
+		relative_expected_vv_source := '${common_relative_path}.vv' // `relative/path/to/test_name.vv`
+		go_source := os.join_path(go2v_path, common_relative_path) + '.go' // `path/to/test_name.go`
+		expected_vv_source := os.join_path(go2v_path, relative_expected_vv_source)
+		full_conversion_command := '$go2v_exe ${os.quoted_path(go_source)} -o ${os.quoted_path(produced_vv_source)}'
+		res := os.execute(full_conversion_command)
+
+		// missing .go file
+		if !os.exists(go_source) {
+			println('${term.bright_magenta('SKIP  ')} $start `$go_source` is missing.')
+			continue
+		}
 
 		// Invalid Go code
 		if res.exit_code != 0 {
-			failures << relative_expected_vv_source
+			failures << full_conversion_command
 			println('${term.bright_red('failed')} $start $relative_expected_vv_source . go2v exited with $res.exit_code code; output:\n$res.output')
 			continue
 		}
 		// `test_name.vv` file missing
 		if !os.exists(expected_vv_source) {
-			failures << relative_expected_vv_source
+			failures << full_conversion_command
 			println('${term.bright_red('failed')} $start $relative_expected_vv_source . $expected_vv_source is missing.')
 			continue
 		}
 		// `out.vv` file missing
 		if !os.exists(produced_vv_source) {
-			failures << relative_expected_vv_source
+			failures << full_conversion_command
 			println('${term.bright_red('failed')} $start $relative_expected_vv_source . $produced_vv_source is missing.')
 			continue
 		}
 		// Go2V generated wrong output
 		if os.read_file(produced_vv_source)? != os.read_file(expected_vv_source)? {
-			failures << relative_expected_vv_source
+			failures << full_conversion_command
 			println('${term.bright_red('failed')} $start $relative_expected_vv_source')
 			println(diff.color_compare_files(diffcmd, expected_vv_source, produced_vv_source))
 			continue
@@ -67,7 +77,7 @@ fn test_all() ? {
 	if failures.len > 0 {
 		eprintln('Summary of test failures:')
 		for f in failures {
-			eprintln('${term.bright_red('failed')} $f')
+			eprintln('${term.bright_red('failed')}: $f')
 		}
 		exit(1)
 	}
